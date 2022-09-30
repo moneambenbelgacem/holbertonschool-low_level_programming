59c1c4cb1501957df5b7d5b6e63b73e15e16eb77
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-09-30 06:48:43 -0700

    betty print

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of double: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of char: %zu byte(s)\n", sizeof(charType));
 	return (0);
 }
