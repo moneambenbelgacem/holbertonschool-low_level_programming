fa5834f3dc144b648f83897254ab36c58ce16ccf
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-09-30 07:09:31 -0700

    betty print

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	printf("Size of int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(floatType));
-	printf("Size of a long long int: %ll byte(s)\n", sizeof(doubleType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(doubleType));
 	printf("Size of char: %zu byte(s)\n", sizeof(charType));
 	return (0);
 }
