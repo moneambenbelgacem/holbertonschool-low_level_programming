32d3c500d9fbf6d541b608016e38eb081d875fdc
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-09-30 05:19:55 -0700

    assembly_etape__

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main(void)
 {
-	char ch[]="\"Programming is like building a multilingual puzzle";
-puts(ch); 
-return 0; 
+char ch[]="\"Programming is like building a multilingual puzzle";
+puts(ch);
+return (0); 
 }
