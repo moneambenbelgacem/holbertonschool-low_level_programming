9b59fffddf72919a68c491db593030654c1cfed5
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-09-30 07:19:02 -0700

    betty print

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int intType;
-	long int floatType;
-	long long int doubleType;
-	float floa;
-	char charType;
+        char charType;
+        int intType;
+        long int floatType;
+        long long int doubleType;
+        float floa;
 
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(floatType));
-	printf("Size of float: %zu bytes\n", sizeof(floa));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(doubleType));
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
-	return (0);
+        printf("Size of char: %zu byte(s)\n", sizeof(charType));
+        printf("Size of int: %zu byte(s)\n", sizeof(intType));
+        printf("Size of a long int: %ld byte(s)\n", sizeof(floatType));
+        printf("Size of a long long int: %d byte(s)\n", sizeof(doubleType));
+        printf("Size of float: %zu bytes\n", sizeof(floa));
+        return (0);
 }
