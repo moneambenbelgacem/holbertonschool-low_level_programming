d328cbe0f022de1ef9274ec9dc003ff2961e269d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-03 05:00:24 -0700

    alpha_expected_e_q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+	if ((al != 'e') && (al != 'q'))
+	putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
