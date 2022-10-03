9f9ffe1619e0cb290abbef3b884b2faa8c797015
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-03 03:35:26 -0700

    alpha

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	putchr('\n');
+	getch();
+	}
+}
