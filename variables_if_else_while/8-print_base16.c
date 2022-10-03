1504651ed2de007bbbabb113014a082108e69ea4
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-03 07:12:34 -0700

    affiche_number_with_putchar

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
