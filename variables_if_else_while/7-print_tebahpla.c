5b57c1b8be3e831f4591e402cd02af209c9e1118
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-03 06:51:08 -0700

    affiche_number_with_putchar

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 122; ch >= 97; ch--)
+		putchar(ch);
+
+	putchar('\n');
+	return (0);	
+}
