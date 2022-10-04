aba8b65dedc9a24b149352b58e386597cffd49dc
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-04 21:52:41 +0200

    print_alpha_x_10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that print lowercase_x_10
+ *
+ */
+void print_alphabet_x10(void)
+{
+    int i,c;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (c = 97; c <= 122; c++)
+         {
+            _putchar(c);
+         }
+         _putchar('\n');
+    }
+}
