a6aa65f040e2156e3625dce091bc4d3795a7513d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-04 22:11:48 +0200

    print_alpha_.

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  */
 void print_alphabet(void)
 {
-    int c;
-    
-    for (c = 97; c <= 122; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+int c;
+
+for (c = 97; c <= 122; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
