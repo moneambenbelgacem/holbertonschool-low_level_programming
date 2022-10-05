0801197aab55c454302a11b7ca20d1d2038ef5d5
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-05 11:49:39 +0200

    print_last_digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - function that print negative or positif check
+ *
+ * @a: variable input
+ * Return:  abs of input.
+ */
+int print_last_digit(int a)
+{
+return (a % 10);
+}
