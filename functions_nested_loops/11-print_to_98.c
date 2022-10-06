9d36e36f24b422eafeaa7d3035d63cb43e4d1431
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-06 13:34:32 +0200

    print_to98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,17 @@ if (n >= 98)
 {
 for (i = n; i >= 98; i--)
 {
-if( i != 98)
+if (i != 98)
 {
-
 printf("%d", i);
 printf(",");
 printf(" ");
-}else
+}
+else
 {
-    printf("%d", i);
+printf("%d", i);
 
-} 
-
+}
 }
 printf("\n");
 }
@@ -33,17 +32,17 @@ for (i = n; i <= 98; i++)
 {
 if (i != 98)
 {
-
 printf("%d", i);
 printf(",");
 printf(" ");
-}else
+}
+else
 {
-    printf("%d", i);
-
-} 
+printf("%d", i);
+}
 }
 printf("\n");
 
 }
 }
+
