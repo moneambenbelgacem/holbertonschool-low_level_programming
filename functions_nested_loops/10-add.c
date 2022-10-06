0272eb531744dd8199c4f65ab9d162e08c0a7551
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-06 12:48:42 +0200

    times_table_matrice

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Entry point
+ *
+ * Description: 'prints every minute of the day of Jack Bauer.'
+ *
+ * @a: first value input
+ * @b: second value input
+ * Return: value
+ */
+int add(int a, int b)
+{
+int res = a + b;
+
+return (res);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,3 +10,4 @@ int _abs(int a);
 int print_last_digit(int a);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
