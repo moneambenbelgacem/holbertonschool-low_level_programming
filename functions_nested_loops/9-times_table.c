22a2573c60f18a5de8eaf7a660452a7dd0ff8d21
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-06 12:00:42 +0200

    times_table_matrice

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,29 +9,23 @@
  */
 void times_table(void)
 {
- 
- int i,j; 
- for (i = 0; i<= 9;i++)
- {
-    for (j=0; j<= 9; j++)
-    {
-       
 
-        printf("%d",i*j);
-        if(j==9)
-        continue;
-        if (i*j < 10)
-        {
-            printf(",  ");
-        }
-        else
-        {
-            printf(", ");
-        }
-       
-    }
-     putchar('\n');
-   
- }
+	int i, j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ((i * j) < 10 && j != 0)
+			{
 
+				putchar(' ');
+			}
+			if (j != 0)
+				putchar(' ');
+			printf("%d", i * j);
+			if (j != 9)
+				putchar(',');
+		}
+		putchar('\n');
+	}
 }
