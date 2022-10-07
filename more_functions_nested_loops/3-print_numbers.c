0554d5bdb2df183cd55ed50097a811020669b18b
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-07 10:07:19 +0200

    is upper or not

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers - function that print negative or positif check
+ *
+ *Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		printf("%d", i);
+	}
+}
+
