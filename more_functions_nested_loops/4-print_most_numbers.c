fba57bf9e96796aa4f4ae16988a10f6a6c9c0b93
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-07 11:52:37 +0200

    is upper or not

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that print negative or positif check
+ * _putchar - writes the character c to stdout
+ *Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if(i != 50 && i != 52)
+		{	
+			_putchar(i);
+		}
+		
+	}
+	_putchar('\n');
+}
