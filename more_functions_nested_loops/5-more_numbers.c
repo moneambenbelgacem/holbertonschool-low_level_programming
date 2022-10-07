69ef92dd8c1a8a5d46568d11be744635a82683fe
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-07 12:20:02 +0200

    is upper or not

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - function that print negative or positif check
+ * _putchar - writes the character c to stdout
+ *Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i < 10)
+		{
+		_putchar(i + '0');
+
+		}
+		else
+		{
+
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+		}
+	}
+	putchar('\n');
+}
