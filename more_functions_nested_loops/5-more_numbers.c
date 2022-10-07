fd15d5b20dc48c198866726a675ac100daac7228
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-07 12:23:53 +0200

    number_10

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * more_numbers - function that print negative or positif check
- * _putchar - writes the character c to stdout
+ * _putchar - writes  the character c to stdout
  *Return: Always 0 (Success)
  */
 void more_numbers(void)
