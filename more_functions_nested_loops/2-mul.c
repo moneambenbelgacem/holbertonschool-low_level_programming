6b11ab00aad1832b884071227855d98392e83cf5
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-07 10:01:52 +0200

    is upper or not

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - function that print negative or positif check
+ *
+ * @a: variable input
+ * @b: variable input
+ * Return: return result multiple
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
