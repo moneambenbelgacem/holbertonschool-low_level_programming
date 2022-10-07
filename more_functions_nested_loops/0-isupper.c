2c5c98130fc35726ccafb1a104b10cb027dd6dcb
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-07 09:44:40 +0200

    is upper or not

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - function that print negative or positif check
+ *
+ * @c: variable input
+ * Return:  1 or 0 is_upper.
+ */
+int _isupper(int c)
+{
+	if((c >= 65)&&(c <= 90))
+	return (1);
+	return (0);
+}
