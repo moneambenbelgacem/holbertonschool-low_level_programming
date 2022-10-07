04dfb96283c56e59f29b0acf9f666d3e245aa13d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-07 16:42:53 +0200

    make_3

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_line - function that print negative or positif check
+ * _putchar - writes the character c to stdout
+ * @n: input for print_line
+ *Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)	
+	{
+	_putchar('\n');
+
+	}else 
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = n-i; j<= n; j++)
+			{
+			_putchar(32);
+			}
+			_putchar(92);
+			_putchar(10);
+
+		}
+		
+	}
+
+}
