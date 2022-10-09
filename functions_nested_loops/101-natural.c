f7cf494e08787ea17613357385dfe220f99952ee
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-09 17:23:25 +0200

    natural

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 1; i <= 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("%d", sum);
+	printf("\n");
+}
