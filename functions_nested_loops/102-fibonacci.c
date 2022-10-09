ce381fb2502d24e06083f62ac29f517732343bf8
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-09 17:49:51 +0200

    FIB._

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	int tab[50], i;
+	int tab[51], i;
 
 	tab[1] = 1;
 	tab[2] = 1;
-	printf("%d, %d\n", tab[1], tab[2]);
+	
 
-	for (i = 3; i <= 50; i++)
-	{
-		tab[i] = tab[i - 1] + tab[i - 2];
-		printf("%d, ", tab[i]);
+	for (i = 2; i <= 50; i++)
+	{	printf("%d, ", tab[i]);
+		tab[i+1] = tab[i] + tab[i-1];
+		
 	}
 
 	return (0);
