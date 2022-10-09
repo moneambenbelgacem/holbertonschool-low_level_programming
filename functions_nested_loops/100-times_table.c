3338e78776413b36273f5c2be188091225635b41
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-09 18:43:55 +0200

    tables

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n) {
 
   for (i = 0; i <= n; i++) {
     for (j = 0; j <= n; j++) {
-      if ((i * j) < 10) {
+      if ((i * j) < 10 && j != 0) {
 
         putchar(',');
         putchar(' ');
--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,42 +7,39 @@
  */
 void print_to_98(int n)
 {
-int i;
-if (n >= 98)
-{
-for (i = n; i >= 98; i--)
-{
-if (i != 98)
-{
-printf("%d", i);
-printf(",");
-printf(" ");
+	int i;
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d", i);
+				printf(",");
+				printf(" ");
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d", i);
+				printf(",");
+				printf(" ");
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+		printf("\n");
+	}
 }
-else
-{
-printf("%d", i);
-
-}
-}
-printf("\n");
-}
-else
-{
-for (i = n; i <= 98; i++)
-{
-if (i != 98)
-{
-printf("%d", i);
-printf(",");
-printf(" ");
-}
-else
-{
-printf("%d", i);
-}
-}
-printf("\n");
-
-}
-}
-
