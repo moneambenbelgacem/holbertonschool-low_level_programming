ff57dd32e742192a92ecc18bc2db50a76fd2c818
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-09 18:40:41 +0200

    tables

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - Entry point
+ *
+ * Description: 'prints every minute of the day of Jack Bauer.'
+ *@n: input value of matrice
+ * Return: void
+ */
+void print_times_table(int n) {
+
+  int i, j;
+
+  for (i = 0; i <= n; i++) {
+    for (j = 0; j <= n; j++) {
+      if ((i * j) < 10) {
+
+        putchar(',');
+        putchar(' ');
+        putchar(' ');
+        putchar(' ');
+      } else if ((i * j) > 9 && (i * j) < 100) {
+
+        putchar(',');
+        putchar(' ');
+        putchar(' ');
+      } else {
+
+        putchar(',');
+        putchar(' ');
+      }
+
+      printf("%d", i * j);
+    }
+    putchar('\n');
+  }
+}
