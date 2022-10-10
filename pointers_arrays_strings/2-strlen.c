b48b19b73b2cb511fdc36de49fd2c774d429c853
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-10 12:17:39 +0200

    return_length

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -6,17 +6,13 @@
  * Return: Always 0.
  */
 int _strlen(char *s)
-{ 
-
-int count = 0 ;
-
-while(s[count] != '\0')
 {
-	count++;
-	
 
-}
-return (count);
+	int count = 0;
 
-
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
 }
