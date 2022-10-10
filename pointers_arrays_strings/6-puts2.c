0eab8854293f903c47068b3296434e1e51828462
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-10 19:17:02 +0200

    saut_pointer

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * print_rev - update the value of n
@@ -9,17 +9,13 @@
 void puts2(char *str)
 {
 	int l, i;
-	
 
-	l = strlen(str)-1;
-	
+	l = strlen(str) - 1;
+
 	for (i = 0; i <= l; i += 2)
 	{
-		
-		printf("%c", str[i]);
-		
 
-		
+		printf("%c", str[i]);
 	}
 	printf("\n");
 }
