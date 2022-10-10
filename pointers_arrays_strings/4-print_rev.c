ed9efe3268081bd2a7b9d743fe51e2f611456c19
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-10 14:49:29 +0200

    print_reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - update the value of n
+ *@s: value input
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int t ;
+	int i; 
+
+	t = strlen(s);
+
+	for (i = t-1; i >= 0; i--)
+	{
+
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
