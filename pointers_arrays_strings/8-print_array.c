9cfd876d7f762add5a778a088ffcb37a13039a9b
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-11 12:06:16 +0200

    affiche_tableau

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - update the value of n
+ *@a: value input
+ *@n: taille du tableau
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i; 
+
+	for(i = 0; i < n; i++)
+	printf("array[%d] = %d\n",i,a[i]);
+	
+}
