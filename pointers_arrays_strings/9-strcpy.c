b08581f02e5a90025d8dc918c142b2097abf23fc
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-11 19:50:52 +0200

    copy_

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * strcpy -  update the value of n
+ * _strcpy -  update the value of n
  *@src: value input
  *@dest: value input
  * Return: Always 0.
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 #include<string.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,4 +9,3 @@ void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-#endif
