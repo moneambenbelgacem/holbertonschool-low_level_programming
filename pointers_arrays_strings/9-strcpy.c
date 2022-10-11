4929a6f2bab8d1c0c80fb0df701f56a77bd74771
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-11 19:41:30 +0200

    copy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i,l;
 
-	for (i = 0; src[i] != '\0'; ++i)
+l = strlen(src);
+	for (i = 0; i <= l; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
 	return (dest);
-	printf("\n");
 }
 
