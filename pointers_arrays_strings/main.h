9d1e45efc61fc2d3d570ba80eeed292e2cbf1ad0
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-11 19:44:33 +0200

    copy

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include<string.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,3 +11,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif
