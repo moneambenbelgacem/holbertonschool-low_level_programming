a82b31604bd825c1b583a477b55184a74a8c4dba
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-11 12:10:43 +0200

    affiche_tableau

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,5 +12,16 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		printf("array[%d] = %d\n", i, a[i]);
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+
+			printf("%d", a[i]);
+		}
+	}
+	printf("\n");
 }
