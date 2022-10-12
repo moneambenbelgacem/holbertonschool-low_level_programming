a2192233abb2b7a45dd3450fdf13aa30b9914f05
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-12 18:04:51 +0200

    copy_n

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strncpy -  update the value of n
+ *@src: value input
+ *@dest: value input
+ *@n: value input
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i;
+
+	for (i = 0; src[i] != '\0' && i <= n; i++)
+	{
+
+		dest[i] = src[i];
+	}
+	while (i < n)
+		dest[i++] = '\0';
+
+	return (dest);
+}
