bc0f5d2bdc888ce39774ee563bfcc73b83ce8c6d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-13 15:38:27 +0200

    to_upper

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - print an array of integers
+ * _putchar - writes the character c to stdout
+ * @str: an array of integers
+ *
+ * Return: nothing.
+ */
+char *leet(char *s)
+{
+
+	int i, j ;
+	char t[5] = {'a', 'e', 'o', 't', 'l'}, T[5] = {'A', 'E', 'O', 'T', 'L'},n[5] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if ((s[i] == t[j]) || (s[i] == T[j]))
+				s[i] = n[j] ;
+		}
+	}
+	return (s);
+}
