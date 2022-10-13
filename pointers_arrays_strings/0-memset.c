f69626234ca083d70689097ffeeff6ec16bef08d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-13 16:45:08 +0200

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - print an array of integers
+ * _putchar - writes the character c to stdout
+ * @s: an input point to buffer
+ * @b: char to plain 
+ *
+ * Return: nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i ;  
+	for ( i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
