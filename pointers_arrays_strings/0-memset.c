09f8ee68cb118f88a1603193dfe2d7cc5380bde5
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-13 17:11:44 +0200

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,15 +5,15 @@
  * _memset - print an array of integers
  * _putchar - writes the character c to stdout
  * @s: an input point to buffer
- * @b: char to plain 
- * @n: char to plain 
+ * @b: char to plain
+ * @n: char to plain
  *
  * Return: nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i ;  
-	for ( i = 0; i < n; i++)
+	unsigned int i;
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
