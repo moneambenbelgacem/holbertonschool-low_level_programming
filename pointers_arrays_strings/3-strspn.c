06e2614e918d7a42308043cdf63ff268d9b10960
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-14 11:20:37 +0200

    strchr_

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strchr - print an array of integers
+ * _strspn - print an array of integers
  * _putchar - writes the character c to stdout
  * @s: char to plain
  * @accept: char to plain
@@ -9,20 +9,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i,j;
+	unsigned int i, j;
 
-	for(i=0; s[i] != '\0'; i++){
-		for(j=0; accept[j] != '\0'; j++){
-			if(s[i] == accept[j])
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
 				break;
 		}
 
-		if(!accept[j])
+		if (!accept[j])
 			break;
-		else{
+		else
+		{
 			j = 0;
 		}
 	}
 
 	return (i);
 }
+
