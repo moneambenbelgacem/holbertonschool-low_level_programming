890d97cccd7ebe3329af5e6d912e835616981b6d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-14 11:13:23 +0200

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - print an array of integers
+ * _putchar - writes the character c to stdout
+ * @s: char to plain
+ * @accept: char to plain
+ * Return: nothing.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i,j;
+
+	for(i=0; s[i] != '\0'; i++){
+		for(j=0; accept[j] != '\0'; j++){
+			if(s[i] == accept[j])
+				break;
+		}
+
+		if(!accept[j])
+			break;
+		else{
+			j = 0;
+		}
+	}
+
+	return (i);
+}
