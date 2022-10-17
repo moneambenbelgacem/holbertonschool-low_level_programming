108ff1223e3975b13b97a799e7e96508c73ecd53
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-17 19:29:00 +0200

    sqrt_implementation

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _pow_recursion - update the value of n
+ * _putchar - puts the char
+ *@n: value input
+ * Return: Always 0.
+ */
+
+int sqrt_check(int x, int y)
+{
+	if (x * x == y)
+	{
+
+		return (x);
+	}
+	else if (x * x > y)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_check(x + 1, y));
+	}
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
+}
