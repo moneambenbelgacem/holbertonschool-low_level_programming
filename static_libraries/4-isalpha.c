8a6ad4cd4cae9b7d7dba6ffd4b9dd9110b5df645
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-18 14:08:56 +0200

    static_lib

--- static_libraries/4-isalpha.c
+++ static_libraries/4-isalpha.c
@@ -1,15 +1,7 @@
 #include "main.h"
-#include <ctype.h>
-/**
- * _isalpha - function that print alpha check
- *
- * @c: variable input
- * Return:  0 or 1.
- */
+
 int _isalpha(int c)
 {
-if (isalpha(c) == 0)
-return (0);
 
 return (1);
 }
