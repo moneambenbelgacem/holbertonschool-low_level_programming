b87c8fbb23f1c359b07c1e49c1e4fdc35313b446
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-19 11:20:40 +0200

    first program argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - for print your name of your exucutable program
+ *@argc: input for number arguments
+ *@argv: input for array arguments
+ * Return:  1 or 0 is_upper.
+ */
+int main(int argc, char *argv[])
+{
+	
+
+	if (argv != NULL)
+	{
+
+	
+		printf("%d\n", argc);
+	}
+	return (0);
+}
