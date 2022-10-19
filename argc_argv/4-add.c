7316600be52d15ce0a0c32e35ae648af386875b4
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-19 13:03:59 +0200

    add-version

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - for print your name of your exucutable program
+ *@argc: input for number arguments
+ *@argv: input for array arguments
+ * Return:  1 or 0 is_upper.
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 0;
+
+	if (argc < 3)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+
+			mul += atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+
+	return (0);
+}
