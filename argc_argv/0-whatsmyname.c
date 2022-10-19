fb0a93e2e0766cadf6bd85226664447efc1aba44
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-19 10:35:52 +0200

    first program argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - for print your name of your exucutable program
+ *
+ * Return:  1 or 0 is_upper.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
