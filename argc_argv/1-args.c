d19fbc43d6075a44a716449a9ec969cf3a9f927d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-19 12:56:30 +0200

    print the argv arguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,17 +5,12 @@
  *@argv: input for array arguments
  * Return:  1 or 0 is_upper.
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__ ((unused)) char *argv[])
 {
-	int i;
+	
 
-	if (argv != NULL)
-	{
-
-		for (i = 0; i < argc - 1; i++)
-		{
-		}
-		printf("%d\n", i);
-	}
+	printf("%d\n", argc - 1);
+	
 	return (0);
 }
+
---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - for print your name of your exucutable program
+ *@argc: input for number arguments
+ *@argv: input for array arguments
+ * Return:  1 or 0 is_upper.
+ */
+int main(int argc, char *argv[])
+{
+	int i,mul=1;  
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+	else{
+		for (i = 1; i<argc; i++)
+		{
+	
+			
+				mul *=atoi(argv[i]);
+			
+		}
+		printf("%d\n",mul);
+	}
+	
+	return (1); 
+}
