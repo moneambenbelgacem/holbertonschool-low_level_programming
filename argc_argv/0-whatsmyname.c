ac38fffcac933ec1663d325df003fe1e309f3851
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-19 10:38:03 +0200

    first program argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - for print your name of your exucutable program
- *
+ *@argc: input for number arguments
+ *@argv: input for array arguments
  * Return:  1 or 0 is_upper.
  */
 int main(int argc, char *argv[])
