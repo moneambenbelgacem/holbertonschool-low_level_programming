701e56a814183c490bd774468df92821a5ee52d5
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-20 20:01:32 +0200

    concat_with_malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,40 +1,39 @@
 #include <string.h>
-#include<string.h>
+#include <string.h>
 #include <stdlib.h>
 
 /**
- * _strdup - function that print negative or positif check
- *@str: length of array
+ * str_concat - function that print negative or positif check
+ *@s1: length of array
+ *@s2: length of array
  *Return: Always 0 (Success)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *chc = NULL; 
-	unsigned int i  , j = 0,y,x =0  ;
+	char *chc = NULL;
+	unsigned int i, j = 0, y, x = 0;
 
-	if(s1 == 0)
+	if (s1 == 0)
 		s1 = "";
 
-	if(s2 ==0)
+	if (s2 == 0)
 		s2 = "";
 
-	chc = malloc(sizeof(char) * (strlen(s1)+strlen(s2)+1));  
+	chc = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+
+	if (chc == 0)
+		return (NULL);
 
 	for (i = 0; i < strlen(s1); i++)
 	{
-		*(chc +i) = *(s1 + j);  
+		*(chc + i) = *(s1 + j);
 		j++;
 	}
-		for (y = i; y < (strlen(s1)+strlen(s2)); y++)
-		{
+	for (y = i; y < (strlen(s1) + strlen(s2)); y++)
+	{
 
-		*(chc +y) = *(s2 + x);
-		x++;  
-		
-		}
-		return (chc);
-
-
-
-
+		*(chc + y) = *(s2 + x);
+		x++;
+	}
+	return (chc);
 }
