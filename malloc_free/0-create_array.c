bccd5ea192906f9771686c8f4966f9cb6d65e3e2
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-20 18:19:38 +0200

    copy strijng with pointer and allloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * create_array - function that print negative or positif check
  *@size: length of array
---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_array - function that print negative or positif check
+ *@size: length of array
+ *@c: input char
+ *Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	char *chp = NULL ;
+	int i = 0, j = 0, len = strlen(str) ;
+
+
+	chp = malloc(sizeof(char) * len);
+
+	while (i < len)
+	{
+		*(chp + j) = *(str + i);
+		i++;
+		j++;
+	}
+
+	return (chp);
+}
