f390f533e84500a9fe1034c96c5295414cbf8040
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-20 17:59:27 +0200

    create tab with malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,13 +4,14 @@
 /**
  * create_array - function that print negative or positif check
  *@size: length of array
- *@c: input char 
+ *@c: input char
  *Return: Always 0 (Success)
  */
 char *create_array(unsigned int size, char c)
 {
 	char *mytab = NULL;
 	unsigned int i;
+
 	mytab = malloc(sizeof(*mytab) * size);
 	if (size == 0)
 		return (NULL);
