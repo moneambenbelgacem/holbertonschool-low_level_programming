439bbfc5c423cef03b0dbab9ee95830444f162d6
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-20 17:52:53 +0200

    create tab with malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,13 +8,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *mytab = malloc(sizeof(*mytab) * size);  
+	char *mytab = NULL;
 	unsigned int i;
-
+mytab = malloc(sizeof(*mytab) * size); 
 	if (size == 0)
 		return (NULL);
 
-	
+	if (!mytab)
+	return(NULL);
 
 	for (i = 0; i < size; i++)
 	{
