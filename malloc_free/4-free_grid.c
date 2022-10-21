8df62374de786172c81820b7a94905d6b5358cff
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-21 14:29:22 +0200

    allocate_matrix_with_malloc

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * alloc_grid - function that print negative or positif check
+ *@width: length of array
+ *@height: length of array
+ *Return: Always 0 (Success)
+ */
+void free_grid(int **grid, int height)
+{
+	int i ; 
+	for (i = 0; i < height; i++)
+	{
+		free ( grid);
+	}
+}
