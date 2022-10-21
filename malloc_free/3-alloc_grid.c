9f7b87a395341e3ec2c2ba693ed0636f44dce8c5
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-21 14:31:29 +0200

    allocate_matrix_with_malloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int *) * height);
+	grid = malloc(sizeof(int ) * height);
 	if (grid == 0)
 	{
 		return (NULL);
