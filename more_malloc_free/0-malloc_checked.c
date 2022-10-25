0925efae564bf36e68c28e1abdf726741086a16d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-25 11:59:19 +0200

    0-malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * number - function to calculate number of words
+ * @str: string being passed to check for words
+ *
+ * Return: number of words
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *x;
+	x = malloc(b);
+	
+	return(x);
+}
