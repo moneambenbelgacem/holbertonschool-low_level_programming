faf7e28d80836d9b08a8ed811e583d276706b1f0
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-25 17:50:35 +0200

    rang_arr

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _calloc - function to calculate number of words
+ *@nmemb: string being passed to check for words
+ *@size: string being passed to check for words
+ * Return: number of words
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int a = 0;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = (int *)malloc(sizeof(int) * max - min + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	while (min <= max)
+	{
+		ptr[a] = min;
+		min++;
+		a++;
+	}
+	return (ptr);
+}
