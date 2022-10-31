33900590a20c72c5a618d083c0c6eceaa25a071a
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-31 18:37:47 +0100

    my_first_struct*

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,19 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+/**
+ * struct dog - dog struct
+ *
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Description: Dog struct
+ */
 struct dog
 {
-	char* name;  
-	float age; 
-	char* owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
 
 #endif
