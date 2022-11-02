e95b33afd53ec467a83aea5155c4e50227930108
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-02 10:25:59 +0100

    check_array_98

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * print_name - Entry point
+ *
+ * @name: string
+ * @f: function
+ *
+ * Description: 'print name.'
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	if ((array != NULL) && size)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
