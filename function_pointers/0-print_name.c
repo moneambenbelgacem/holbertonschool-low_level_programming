1b5b526afa03adb907eb4c1ef65e4bee33e4bd9d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-02 09:18:15 +0100

    first_function_pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * print_name - Entry point
+ *
+ * @name: string
+ * @f: function
+ *
+ * Description: 'print name.'
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if ((f != NULL) && (name != NULL) )
+	{
+
+		f(name) ;
+	}
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
