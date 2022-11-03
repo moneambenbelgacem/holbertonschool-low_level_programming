e2577730385b1076a9b536ef40e9223386cb8539
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-03 10:05:42 +0100

    calculatrice

--- function_pointers/0-main.c
+++ function_pointers/0-main.c
@@ -1,57 +1,40 @@
+#include "3-calc.h"
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include "function_pointers.h"
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
+ * main - entry point for function
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args
  *
- * Return: 0 if false, something else otherwise.
- */
-int is_98(int elem)
-{
-    return (98 == elem);
-}
-
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
+ * Description: calculates basic arithmetic
  *
- * Return: 0 if false, something else otherwise.
+ * Return: 0 on success, 98 on failure
  */
-int is_strictly_positive(int elem)
+int main(int argc, char *argv[])
 {
-    return (elem > 0);
-}
+	char op;
+	int (*calc_func)(int, int);
+	int result;
 
-
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-    return (elem == 98 || -elem == 98);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
-
-    index = int_index(array, 20, is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
-    printf("%d\n", index);
-    return (0);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	op = *(argv[2]) ;
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
+	{
+		printf("Error\n");
+		return (100);
+	}
+	calc_func = get_op_func(&op);
+	if (calc_func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	result = calc_func(atoi(argv[1]),atoi(argv[3]));
+	printf("%d\n", result);
+	return (0);
 }
