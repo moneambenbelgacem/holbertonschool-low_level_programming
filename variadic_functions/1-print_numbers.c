00da55073741ca0b4a68b0df96ad89b84c37dc2b
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-03 18:28:15 +0100

    print_all

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Entry point
+ *
+ * @n: string
+ *
+ * Description: 'print name.'
+ *
+ * Return: number
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int x;
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+	for (i = 1; i <= n; i++)
+	{
+		x = va_arg(args, int);
+		if (separator)
+		{
+
+			if (i == n)
+			{
+
+				printf("%d", x);
+			}
+			else
+			{
+
+				printf("%d%s", x, separator);
+			}
+		}
+	}
+	printf("\n");
+}
