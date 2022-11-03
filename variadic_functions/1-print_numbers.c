e87556d0e5fae605325fcb1e8ca8f9a687b452b1
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-03 18:29:15 +0100

    print_all

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 1; i <= n; i++)
 	{
 		x = va_arg(args, int);
-		if (separator)
+		if (separator != NULL)
 		{
 
 			if (i == n)
