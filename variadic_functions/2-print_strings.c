f009341d33a7dc00ddc0087f6b1e421c9ec2c9d6
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-04 09:59:09 +0100

    print aruments of strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - Entry point
+ *
+ * @separator: string
+ * @n: nuber of elem
+ *
+ * Description: 'print name.'
+ *
+ * Return: number
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	 char *x ;
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+	for (i = 1; i <= n; i++)
+	{
+		x = va_arg(args, char *);
+
+		if (x == NULL )
+		printf("nil\n");
+		
+		if (separator != NULL)
+		{
+
+			if (i == n)
+			{
+
+				printf("%s", x);
+			}
+			else
+			{
+
+				printf("%s%s", x, separator);
+			}
+		}
+		else
+		{
+			printf("%s", x);
+		}
+	}
+	printf("\n");
+}
