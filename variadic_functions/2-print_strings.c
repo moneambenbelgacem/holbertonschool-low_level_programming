c56621dc5ce9de0e805d2a46e953e492ae15c74c
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-04 10:06:15 +0100

    print aruments of strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("(nil)");
 		}
 		
-		if (separator != NULL && x != NULL)
+		else if (separator != NULL && x != NULL)
 		{
 
 			if (i == n)
