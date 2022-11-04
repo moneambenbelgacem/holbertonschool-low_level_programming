991f2927928b992b6c9188387e0e7205de685bb8
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-04 14:45:50 +0100

    print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,40 +3,31 @@
  * print_int - prints an int
  * @args: the list of args
  */
-void print_int(va_list args)
-{
-	printf("%d",va_arg(args, int));
-}
+void print_int(va_list args) { printf("%d", va_arg(args, int)); }
 /**
  * print_char - prints an int
  * @args: the list of args
  */
-void print_char(va_list args)
-{
-	printf("%c",va_arg(args,int));
-}
+void print_char(va_list args) { printf("%c", va_arg(args, int)); }
 /**
  * print_string - prints an int
  * @args: the list of args
  */
-void print_string(va_list args)
-{
-	char *z = va_arg(args , char  *); 
+void print_string(va_list args) {
+  char *z = va_arg(args, char *);
 
-	if (!z)
-	{
-		printf("(nil)"); 
-	}
-	printf("%s",z);
+  if (!z) {
+    printf("(nil)");
+  }
+  else {
+  	printf("%s", z);
+  }
 }
 /**
  * print_float - prints an int
  * @args: the list of args
  */
-void print_float(va_list args)
-{
-	printf("%f",va_arg(args, double));
-}
+void print_float(va_list args) { printf("%f", va_arg(args, double)); }
 
 /**
  * print_strings - Entry point
@@ -48,34 +39,28 @@ void print_float(va_list args)
  *
  * Return: number
  */
-void print_all(const char * const format, ...)
-{
-	int count1 = 0 , count2  ;
-	char *sep1 ="", *sep2 = ", ";
-	va_list args; 
-	my_type tab[] = {
-		{'c',print_char},
-		{'i',print_int},
-		{'f',print_float},
-		{'s',print_string}
-	}; 
-	va_start(args, format);
-	while(format != NULL && format[count1] !=  '\0')
-	{
-		count2 = 0  ;
-		while(tab[count2].z != '\0')
-		{
-			if (tab[count2].z == format[count1])
-			{
-				printf("%s", sep1);
-				tab[count2].f(args);
-				sep1 = sep2;
-			}
-			count2++; 
-		}
-		count1++; 
+void print_all(const char *const format, ...) {
+  int count1 = 0, count2;
+  char *sep1 = "", *sep2 = ", ";
+  va_list args;
+  my_type tab[] = {{'c', print_char},
+                   {'i', print_int},
+                   {'f', print_float},
+                   {'s', print_string}};
+  va_start(args, format);
+  while (format != NULL && format[count1] != '\0') {
+    count2 = 0;
+    while (tab[count2].z != '\0') {
+      if (tab[count2].z == format[count1]) {
+        printf("%s", sep1);
+        tab[count2].f(args);
+        sep1 = sep2;
+      }
+      count2++;
+    }
 
-	}
-	printf("\n");
-
+    count1++;
+  }
+  printf("\n");
+  va_end(args);
 }
