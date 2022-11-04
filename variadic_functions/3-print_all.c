cc9485db3fcf1e04e6f8f254d6b7b49949ec7112
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-04 14:55:25 +0100

    print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,10 +13,11 @@ void print_char(va_list args) { printf("%c", va_arg(args, int)); }
  * print_string - prints an int
  * @args: the list of args
  */
-void print_string(va_list args) {
-  char *z = va_arg(args, char *);
+void print_string(va_list args)
+{
+	char *z = va_arg(args, char *);
 
-  !z?printf("(nil)"):printf("%s",);
+	!z ? printf("(nil)") : printf("%s", z);
 }
 /**
  * print_float - prints an int
@@ -25,37 +26,41 @@ void print_string(va_list args) {
 void print_float(va_list args) { printf("%f", va_arg(args, double)); }
 
 /**
- * print_strings - Entry point
+ * print_all - Entry point
  *
- * @separator: string
- * @n: nuber of elem
+ * format: string
  *
  * Description: 'print name.'
  *
  * Return: number
  */
-void print_all(const char *const format, ...) {
-  int count1 = 0, count2;
-  char *sep1 = "", *sep2 = ", ";
-  va_list args;
-  my_type tab[] = {{'c', print_char},
-                   {'i', print_int},
-                   {'f', print_float},
-                   {'s', print_string}};
-  va_start(args, format);
-  while (format != NULL && format[count1] != '\0') {
-    count2 = 0;
-    while (tab[count2].z != '\0') {
-      if (tab[count2].z == format[count1]) {
-        printf("%s", sep1);
-        tab[count2].f(args);
-        sep1 = sep2;
-      }
-      count2++;
-    }
 
-    count1++;
-  }
-  printf("\n");
-  va_end(args);
+void print_all(const char *const format, ...)
+{
+	int count1 = 0, count2;
+	char *sep1 = "", *sep2 = ", ";
+	va_list args;
+	my_type tab[] = {{'c', print_char},
+					 {'i', print_int},
+					 {'f', print_float},
+					 {'s', print_string}};
+	va_start(args, format);
+	while (format != NULL && format[count1] != '\0')
+	{
+		count2 = 0;
+		while (tab[count2].z != '\0')
+		{
+			if (tab[count2].z == format[count1])
+			{
+				printf("%s", sep1);
+				tab[count2].f(args);
+				sep1 = sep2;
+			}
+			count2++;
+		}
+
+		count1++;
+	}
+	printf("\n");
+	va_end(args);
 }
