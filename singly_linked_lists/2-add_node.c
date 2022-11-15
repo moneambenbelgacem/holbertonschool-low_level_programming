f7ef609ceff7913d565ccfad94df88622ab27a93
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-15 12:51:27 +0100

    add_start

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_list - check the code
+ *@h: list entry point
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len= strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
