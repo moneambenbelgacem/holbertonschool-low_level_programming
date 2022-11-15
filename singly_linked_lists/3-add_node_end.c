693e0c70826f55c4c85c9f5ced086b0bc616ed10
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-15 13:35:28 +0100

    add_end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_node - check the code
+ *@head: list entry point
+ *@str: list entry point
+ * Return: Always 0.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *c;
+
+	new = malloc(sizeof(list_t));
+	if ((new == NULL) || (str == NULL))
+		return (NULL);
+
+	new->len = strlen(str);
+	new->str = strdup(str);
+	new->next = NULL;
+
+	if (*head)
+	{
+		c = *head;
+		while (c->next != NULL)
+			c = c->next;
+		c->next = new;
+	}
+	else
+		*head = new;
+	return (new);
+}
