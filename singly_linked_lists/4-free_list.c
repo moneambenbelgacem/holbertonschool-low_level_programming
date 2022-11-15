aebd74c72d42f306dd8f056f9685764d87f6e181
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-15 14:20:50 +0100

    free_all

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_list - check the code
+ *@h: list entry point
+ * Return: Always 0.
+ */
+void free_list(list_t *head)
+{
+	list_t *victime;
+
+	while (head != NULL)
+	{
+		victime = head;
+		head = head->next; 
+		free(victime);
+	}
+	
+}
