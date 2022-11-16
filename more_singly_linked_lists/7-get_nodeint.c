48e2fc6c98d7ceca8c7f694e2b5dc00dde2f72c9
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 13:04:41 +0100

    get_node

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * get_nodeint_at_index - check the code
+ *@head: list entry point
+ * Return: Always 0.
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *hsol;
