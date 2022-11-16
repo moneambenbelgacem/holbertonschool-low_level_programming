3c679a9b692a5aab5fbca3063f5b1ea4c6248ac3
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 13:55:01 +0100

    insert_node_in_index

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - check the code
+ *@head: list entry point
+ *@index: list entry point
+ * Return: Always 0.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *hsol,*new;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	return (NULL);
+
+	new->n = n; 
+
+	hsol = *head;
+
+	while (hsol)
+	{
+		if (i == idx)
+			break;
+		hsol = hsol->next;
+		i++;
+	}
+	new->next= hsol->next;
+	hsol->next = new;
+
+	return (*head);
+}
