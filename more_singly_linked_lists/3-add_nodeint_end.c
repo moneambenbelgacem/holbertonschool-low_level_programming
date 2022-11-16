813ea4294f24159c2c777c558938054867e68d48
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 09:25:51 +0100

    add_node_end_

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new, *c;
 
 	new = malloc(sizeof(listint_t));
-	if ((new == NULL))
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
