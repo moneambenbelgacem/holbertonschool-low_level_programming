ae4cdac63af36363abd0140821c8cc29b052122a
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 16:52:34 +0100

    insert_node_in_ind

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,6 +10,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *hsol, *new;
 	unsigned int i = 0;
+
 	if (i < idx && *head == NULL)
 	{
 		return (NULL);
