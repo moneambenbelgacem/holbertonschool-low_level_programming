4b907f47012d1716ff2e347a65d640d8f8cb86dd
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 09:17:11 +0100

    add_node_end

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_nodeint - check the code
+ *@head: list entry point
+ *@n: list entry point
+ * Return: Always 0.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new, *c;
+
+	new = malloc(sizeof(listint_t));
+	if ((new == NULL))
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head)
+	{
+		c = *head;
+		while (c->next != NULL)
+			c = c->next;
+		c->next = new;
+	}
+	else
+		*head = new;
+	return (new);
+}
