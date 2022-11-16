161f8b75abef9117d02deb7a9876e2393c26bdc7
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 11:07:43 +0100

    free_head_null

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint - check the code
+ *@head: list entry point
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *victime;
+	if (  *head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		victime = *head;
+		*head = (*(head))->next;
+		free(victime);
+	}
+	*head = NULL;
+}
+
