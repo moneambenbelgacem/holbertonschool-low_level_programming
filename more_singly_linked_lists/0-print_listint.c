e11dd9bea713d570da9a4facf29b042111e4801e
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 08:43:08 +0100

    print_int

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_list - check the code
+ *@h: list entry point
+ * Return: Always 0.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		
+			printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+hello more linked list
