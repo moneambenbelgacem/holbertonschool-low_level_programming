1655955319d4027b22ec8de98b4b16bfd18e6318
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-16 11:09:39 +0100

    free_head_null_

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		victime = *head;
 		*head = (*(head))->next;
