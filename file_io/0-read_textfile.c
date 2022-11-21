d05050884526e25ddb4d5fbc0f4d2e378e3d9d3e
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-21 17:17:12 +0100

    first file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,33 +3,32 @@
 /**
  * read_textfile - check the code
  *@filename: nom du fichier
- *@letters: the size 
+ *@letters: the size
  * Return: Always 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *ptr = NULL; 
-	char ch; 
-	size_t i = 0 ;
+	FILE *ptr;
+	char ch;
+	size_t i = 1;
+	
 
-	ptr = fopen(filename,"r"); 
-	if(ptr == NULL|| letters == 0)
+	ptr = fopen(filename, "r");
+	if (ptr == NULL || letters == 0)
 	{
-		return (0); 
-	}
-
-	while(ch != EOF)
-	{
-		ch = fgetc(ptr);
-		printf("%c",ch);
-		i++; 
-		if(i == letters)
-		break;
-
+		return (0);
 	}
 
 	
+	while ((ch = fgetc(ptr)) != EOF){
+		
+		printf("%c",ch);
+		i++;
+		if(i == letters)
+		break;
+		
+	}
 
-	fclose(ptr) ;
-	return(i);
+	fclose(ptr);
+	return (i);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,7 @@
 #include <stddef.h>
 #include <stdio.h>
+#include<string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #ifndef HEADER_FILE
