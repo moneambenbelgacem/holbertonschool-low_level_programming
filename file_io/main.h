3e8dc47df15a024f56a32643a9bdc36013b6e73e
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-22 12:14:11 +0100

    first file_

--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 #include <stddef.h>
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#ifndef HEADER_FILE
-#define HEADER_FILE
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
+
