1af5d189d3123191bbc92bcb01be8a8e3493184b
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-23 15:44:28 +0100

    append

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,7 +7,7 @@
 #define BUF_SIZE 5000
 int main(int argc, char *argv[])
 {
-	int src, dst, in, out,c1,c2;
+	int src, dst, in, out, c1, c2;
 	char buf[BUF_SIZE];
 	if (argc != 3)
 		exit(1);
@@ -21,28 +21,26 @@ int main(int argc, char *argv[])
 	dst = creat(argv[2], MODE);
 	if (dst < 0)
 		exit(3);
-	
+	while (1)
+	{
 		in = read(src, buf, BUF_SIZE);
-		if (in <= 0)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+		if (in == -1)
+			break;
 		out = write(dst, buf, in);
 		if (out <= 0)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	
-	c1 =close(src);
-	if(c1== -1)
+	}
+	c1 = close(src);
+	if (c1 == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", c1);
 		exit(100);
 	}
 	c2 = close(dst);
-	if(c2== -1)
+	if (c2 == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", c2);
 		exit(100);
