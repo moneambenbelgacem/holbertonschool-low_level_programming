e46d50a9c58391ea1085b7acacfd42a1f29a428e
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-23 13:34:26 +0100

    append

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	src = open(argv[1], O_RDONLY);
 	if (src < 0)
 		exit(2);
-	dst = creat(argv[2], MODE);
+	dst = open(argv[2], O_CREAT  | O_TRUNC,0664);
 	if (dst < 0)
 		exit(3);
 	while (1)
