1a748b8405925f809a6a69d6f3784c10e1743a54
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-23 10:55:38 +0100

    creat_file_if_not_exist

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - check the code
+ *@filename: nom du fichier
+ *@text_content: the content
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content){
+	int fp;
+
+	fp = open(filename,O_WRONLY |O_APPEND);
+	if (fp == -1)
+	{
+		return (-1);
+	}
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	write(fp, text_content, strlen(text_content));
+
+	close(fp);
+	return (1);
+}
