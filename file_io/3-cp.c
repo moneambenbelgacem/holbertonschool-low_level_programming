6a997c0b10b69a92afd7cbb41e3a19cf46debcef
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-23 13:06:54 +0100

    append

---
+++ file_io/3-cp.c
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define MODE 0666
+#define BUF_SIZE 8192
+void main(int argc, char *argv[])
+{
+	int src, dst, in, out;
+	char buf[BUF_SIZE];
+	if (argc != 3)
+		exit(1);
+	src = open(argv[1], O_RDONLY);
+	if (src < 0)
+		exit(2);
+	dst = creat(argv[2], MODE);
+	if (dst < 0)
+		exit(3);
+	while (1)
+	{
+		in = read(src, buf, BUF_SIZE);
+		if (in <= 0)
+			break;
+		out = write(dst, buf, in);
+		if (out <= 0)
+			break;
+	}
+	close(src);
+	close(dst);
+	exit(0);
+}
