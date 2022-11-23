d278443939608ff24d40d1f055082a6b0d795cd1
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-23 09:14:02 +0100

    creat_file_if_not_exist

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,8 +10,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int fp;
 
-	fp = open(filename, O_CREAT | O_WRONLY| O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
 	{
 		return (-1);
 	}
