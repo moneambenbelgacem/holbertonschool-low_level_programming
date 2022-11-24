67b56168fcfe39f58b0a5b9ed1c860a498e695f0
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-24 11:37:48 +0100

    free

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - check the code
+ *@head: input list
+ * Return: Always EXIT_SUCCESS.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *victime = head;
+	while (victime)
+	{
+		dlistint_t *next = victime->next;
+		free(victime);
+		victime = next;
+	}
+}
