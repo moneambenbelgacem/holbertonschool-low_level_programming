0108790f4cd66f9008686f727a8c2c373795b336
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-24 13:45:35 +0100

    sum_nodes_values

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - check the code
+ *@head: input list
+ *@index: input list
+ * Return: Always EXIT_SUCCESS.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *emchi;
+	int sum = 0;
+
+	emchi = head;
+	while (emchi != NULL)
+	{
+		sum += emchi->n;
+		emchi = emchi->next;
+	}
+	return (sum);
+}
