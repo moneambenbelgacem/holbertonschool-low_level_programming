6458b84894d9e4c1a0d1ca151a8d051aa504644d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-24 08:03:52 +0100

    print_double_linked_list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_dlistint - check the code
+ *@h: input list
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	size_t i = 0;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+
+	return (i);
+}
