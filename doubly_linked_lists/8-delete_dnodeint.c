0faa7d5605526dce5243840db0783d2b9f200e81
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-25 16:10:16 +0100

    fixi

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,60 +1,41 @@
 #include "lists.h"
+
 /**
- * delete_dnodeint_at_index - check the code .
- * @head: Argumetns counter
- * @index: Arguments array
- * Return: Always 0.
+ * delete_dnodeint_at_index - delete element by index
+ * @head: address node list
+ * @index: unsigned integer
+ * Return: integer
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *emchi_wrah, *emchi;
-	unsigned int i = 1;
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	tmp = *head;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	emchi_wrah = *head;
-	emchi = (*head)->next;
+
 	if (index == 0)
 	{
-		if ((*head)->next != NULL)
-		{
-			(*head) = (*head)->next;
-			(*head)->prev = NULL;
-			free(emchi_wrah);
-			emchi_wrah = NULL;
-			return (1);
-		}
-		else
-		{
-			free(*head);
-			(*head) = NULL;
-			return (1);
-		}
-	}
-	else
-	{
-		while (i < index )
-		{
-			if (i == index - 1)
-			{
-				emchi_wrah->next = emchi->next;
-				emchi->next->prev = emchi_wrah;
-				free(emchi);
-				emchi = NULL;
-				return (1);
-			}
-			else
-			{
-				if (!emchi)
-					return (-1);
-				emchi = emchi->next;
-				emchi_wrah = emchi_wrah->next;
-				i++;
-			}
-		}
-		emchi_wrah->next = NULL;
-		free(emchi);
-		emchi = NULL;
+		if (tmp->next)
+			tmp->next->prev = NULL;
+		*head = (*head)->next;
+		free(tmp);
 		return (1);
 	}
+
+	while (tmp && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (tmp == NULL)
+		return (-1);
+	tmp->prev->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+	free(tmp);
+	return (1);
 }
