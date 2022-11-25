c9bc8dc49f903960b230be5ee2af3519487e6a77
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-25 15:09:25 +0100

    delete_node_double

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+/**
+ * main - check the code .
+ * @argc: Argumetns counter
+ * @argv: Arguments array
+ * Return: Always 0.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *emchi_wrah, *emchi;
+	unsigned int i = 1;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	emchi_wrah = *head;
+	emchi = (*head)->next;
+	if (index == 0)
+	{
+		if ((*head)->next != NULL)
+		{
+
+			(*head) = (*head)->next;
+			(*head)->prev = NULL;
+			free(emchi_wrah);
+			emchi_wrah = NULL;
+			return (1);
+		}
+		else
+		{
+			free(*head);
+			(*head) = NULL;
+			return (1);
+		}
+	}
+	else
+	{
+		while (emchi->next != NULL)
+		{
+			if (i == index - 1)
+			{
+				emchi_wrah->next = emchi->next;
+				emchi->next->prev = emchi_wrah;
+				free(emchi);
+				emchi = NULL;
+				return (1);
+			}
+			else
+			{
+				emchi = emchi->next;
+				emchi_wrah = emchi_wrah->next;
+				i++;
+			}
+		}
+		emchi_wrah->next = NULL;
+		free(emchi);
+		emchi = NULL;
+		return (1);
+	}
+}
