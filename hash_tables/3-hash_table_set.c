c7e4636a68a664d7815275a6076cebca8e16db02
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-28 14:46:16 +0100

    insert_collision

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (ht->array[add]->key == key)
 		{
 			free(ht->array[add]->value);
-			ht->array[add]->value = value;
+			ht->array[add]->value = strdup(value);
 
 		}
 		else
