1d2dbc3a5d24f3e70b4ae082463e4bff0ed10187
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-28 16:40:14 +0100

    get_element

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,27 +1,25 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - delete element by index
+ * hash_table_get - delete element by index
  * @ht: address node list
  * @key: key entry
- * @value: value entry
  * Return: integer
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int add;
 
-if(ht == NULL || key == NULL)
-return (NULL);
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
-add = key_index((const unsigned char *)key, ht->size);
-while (ht->array[add])
-{
-if(ht->array[add] != NULL && (strcmp(ht->array[add]->key,key) == 0))
-{
-	return(ht->array[add]->value);
-	
-}
-ht->array[add] = ht->array[add]->next;
-}
-return(NULL);
+	add = key_index((const unsigned char *)key, ht->size);
+	while (ht->array[add])
+	{
+		if (ht->array[add] != NULL && (strcmp(ht->array[add]->key, key) == 0))
+		{
+			return (ht->array[add]->value);
+		}
+		ht->array[add] = ht->array[add]->next;
+	}
+	return (NULL);
 }
