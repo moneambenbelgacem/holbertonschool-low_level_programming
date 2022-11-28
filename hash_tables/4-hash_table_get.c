b00cf50b433304f5d73dc7d85aef7cf2d434386f
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-28 16:37:46 +0100

    get_element

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -8,12 +8,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	int add; 
+	int add;
+
+if(ht == NULL || key == NULL)
+return (NULL);
 
 add = key_index((const unsigned char *)key, ht->size);
+while (ht->array[add])
+{
 if(ht->array[add] != NULL && (strcmp(ht->array[add]->key,key) == 0))
 {
 	return(ht->array[add]->value);
+	
+}
+ht->array[add] = ht->array[add]->next;
 }
 return(NULL);
 }
