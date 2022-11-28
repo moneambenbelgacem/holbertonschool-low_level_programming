588ac362d995028eefb0c8100bf39fd99d67c7eb
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-28 11:21:27 +0100

    hash_function

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+/**
+ * key_index - implementation of the djb2 algorithm
+ * @size: string used to generate hash value
+ * @key: string used to generate hash value
+ * Return: hash value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int res;
+
+	res = hash_djb2(key) % size;
+	return (res);
+}
