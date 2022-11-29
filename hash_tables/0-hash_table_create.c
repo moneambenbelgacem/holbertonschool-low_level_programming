e1df8fac567d211d40e8507a80311b05de8c1d2d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-11-29 10:57:20 +0100

    free

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
+	ht->array = malloc(sizeof(hash_node_t *) * size);;
 	if (ht->array == NULL)
 		return (NULL);
 	return (ht);
